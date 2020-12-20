4171c755ab4045c4549dd87b1c039f006a843403
neginkheirmand <neginkheirmand@gmail.com>
2020-12-20 16:59:46 +0330

    added input func

--- MiniProject2.py
+++ MiniProject2.py
@@ -4,18 +4,25 @@ import os
 
 def LoadImage(stringName):
     #this function will load an image with the specified name in the current directory
-    img = cv.imread(os.getcwd()+"/TestCase1.png",1)
+    img = cv.imread(os.getcwd()+"/"+stringName,1)
+    #check if the image exists and can be loaded
     if img is None:
         sys.exit("Could not read the image.")
         return None
-    #if the img exist and can be loaded then will be shown in th window named "before" 
+    #if the img exist and can be loaded then will be shown in the window named "before" 
     cv.imshow("before", img)
     return img
 
-def main():
-    LoadImage("TestCase2.png")
 
-    # print(img)
+
+def main():
+    nameImage = input("Enter the name of the image: ") 
+    image = LoadImage(nameImage)
+    if image==None:
+        #end the program here 
+        return
+    
+    height, width, depth = image.shape
     # print(img.shape)
     # cv.imshow("Display window", img)
     k = cv.waitKey(0)
